26abc957e6483137e613101e7e678116ca0466c3
Milán Nagy <123.wizek@gmail.com>
2016-09-10 20:04:08 +0200

    simplify .> indent

--- test/GradualSpec.hs
+++ test/GradualSpec.hs
@@ -21,21 +21,20 @@ inj
 aMock = 33
 
 spec = do
-  describe "" $ do
-    specify "dependencies" $ do
-      removeIname "aI" `shouldBe` "a"
-      removeIname "aasdasdI" `shouldBe` "aasdasd"
-      (evaluate $ removeIname "a") `shouldThrow` anyException
-      (evaluate $ removeIname "I") `shouldThrow` anyException
+  specify "dependencies" $ do
+    removeIname "aI" `shouldBe` "a"
+    removeIname "aasdasdI" `shouldBe` "aasdasd"
+    (evaluate $ removeIname "a") `shouldThrow` anyException
+    (evaluate $ removeIname "I") `shouldThrow` anyException
 
-    specify "injG should allow for seamless introduction" $ do
-      G.a `shouldBe` 1
-      G.b `shouldBe` 2
-      G.c `shouldBe` 3
-      $( bD $> assemble ) `shouldBe` 2
-      $( bD $> override "a" "aMock" $> assemble ) `shouldBe` 34
-      $( dD $> override "a" "aMock" $> assemble ) `shouldBe` 37
-      G.aA `shouldBe` 1
+  specify "injG should allow for seamless introduction" $ do
+    G.a `shouldBe` 1
+    G.b `shouldBe` 2
+    G.c `shouldBe` 3
+    $( bD $> assemble ) `shouldBe` 2
+    $( bD $> override "a" "aMock" $> assemble ) `shouldBe` 34
+    $( dD $> override "a" "aMock" $> assemble ) `shouldBe` 37
+    G.aA `shouldBe` 1
 
 runOnlyPrefix = [""]
 specify a = if (any (`isPrefixOf` a) runOnlyPrefix)
--- test/MainSpec.hs
+++ test/MainSpec.hs
@@ -21,162 +21,161 @@ inj
 testIdiomaticImportMock = 44
 
 spec = do
-  describe "" $ do
-    specify "mapDepNames" $ do
+  specify "mapDepNames" $ do
 
-      let l x = Dep x []
+    let l x = Dep x []
 
-      mapDepNames (const "2" ) (Dep "1" []) `shouldBe` (Dep "2" [])
-      mapDepNames (const 2) (Dep "1" []) `shouldBe` (Dep 2 [])
-
-
-    specify "convertDepsToExp" $ do
-
-      -- let ppsB = shouldBeF pprint
-      -- let ppsB = shouldBeF show
-      let ppsB = shouldBe
-
-      convertDepsToExp (Dep "a" []) `ppsB` (VarE $ mkName "a")
-
-      convertDepsToExp (Dep "a" [Dep "b" []]) `ppsB`
-        (AppE (VarE $ mkName "a") (VarE $ mkName "b"))
-
-      convertDepsToExp (Dep "a" [Dep "b" [], Dep "c" []]) `ppsB`
-        (AppE (AppE (VarE $ mkName "a") (VarE $ mkName "b")) (VarE $ mkName "c"))
+    mapDepNames (const "2" ) (Dep "1" []) `shouldBe` (Dep "2" [])
+    mapDepNames (const 2) (Dep "1" []) `shouldBe` (Dep 2 [])
 
 
-    specify "override fn" $ do
+  specify "convertDepsToExp" $ do
 
-      override "a" "b" (Dep "a" []) `shouldBe` Rep "b" []
-      override "a" "c" (Dep "a" []) `shouldBe` Rep "c" []
-      -- override "b" "c" (Dep "a" []) `shouldBe` Dep "a" []
-      evaluate (override "b" "c" (Dep "a" [])) `shouldThrow` anyException
+    -- let ppsB = shouldBeF pprint
+    -- let ppsB = shouldBeF show
+    let ppsB = shouldBe
 
-      -- override "x" "c" (Dep "b" [Dep "a" []]) `shouldBe` (Dep "b" [Dep "a" []])
-      evaluate (override "x" "c" (Dep "b" [Dep "a" []])) `shouldThrow` anyException
-      override "b" "c" (Dep "b" [Dep "a" []]) `shouldBe` (Rep "c" [Dep "a" []])
-      override "a" "c" (Dep "b" [Dep "a" []]) `shouldBe` (Dep "b" [Rep "c" []])
+    convertDepsToExp (Dep "a" []) `ppsB` (VarE $ mkName "a")
 
-      override "a" "c" (Dep "b" [Dep "a" [], Dep "a" []]) `shouldBe`
-        (Dep "b" [Rep "c" [], Rep "c" []])
+    convertDepsToExp (Dep "a" [Dep "b" []]) `ppsB`
+      (AppE (VarE $ mkName "a") (VarE $ mkName "b"))
 
-
-    specify "assemble" $ do
-
-      $(assemble barD) `shouldBe` 2
+    convertDepsToExp (Dep "a" [Dep "b" [], Dep "c" []]) `ppsB`
+      (AppE (AppE (VarE $ mkName "a") (VarE $ mkName "b")) (VarE $ mkName "c"))
 
 
-    specify "mocking" $ do
+  specify "override fn" $ do
 
-      let
-        fooDMock = Dep "fooMock" []
-        fooMock = 33
-      $(assemble $ override "foo" "fooMock" barD) `shouldBe` 34
+    override "a" "b" (Dep "a" []) `shouldBe` Rep "b" []
+    override "a" "c" (Dep "a" []) `shouldBe` Rep "c" []
+    -- override "b" "c" (Dep "a" []) `shouldBe` Dep "a" []
+    evaluate (override "b" "c" (Dep "a" [])) `shouldThrow` anyException
 
+    -- override "x" "c" (Dep "b" [Dep "a" []]) `shouldBe` (Dep "b" [Dep "a" []])
+    evaluate (override "x" "c" (Dep "b" [Dep "a" []])) `shouldThrow` anyException
+    override "b" "c" (Dep "b" [Dep "a" []]) `shouldBe` (Rep "c" [Dep "a" []])
+    override "a" "c" (Dep "b" [Dep "a" []]) `shouldBe` (Dep "b" [Rep "c" []])
 
-    specify "type variable support" $ do
-
-      $(assemble $ idTestD) `shouldBe` 3
-
-      let
-        idDMock = Dep "idMock" []
-        idMock = (+1)
-      $(assemble $ override "id" "idMock" idTestD) `shouldBe` 4
+    override "a" "c" (Dep "b" [Dep "a" [], Dep "a" []]) `shouldBe`
+      (Dep "b" [Rep "c" [], Rep "c" []])
 
 
-    specify "module support" $ do
-      $(assemble $ testModuleD) `shouldBe` 12
+  specify "assemble" $ do
 
-    -- specify "qualified names" $ do
-    --   $(assemble $ Dep "Prelude.id" []) 1 `shouldBe` 1
-    --   $(assemble $ Dep "Prelude.*" []) 2 3 `shouldBe` 6
+    $(assemble barD) `shouldBe` 2
 
 
-    specify "code that is more real-life" $ do
+  specify "mocking" $ do
 
-      let parseTime t =
-            fromJust $ parseTimeM True defaultTimeLocale "%Y-%m-%d %H:%M:%S%Q" t
-      mockConsole <- newIORef []
-      cTime <- newIORef $ parseTime "2016-01-01 14:00:00"
-      let
-        -- $(inj)
-        putStrLnMockD = Dep "putStrLnMock" []
-        putStrLnMockT = putStrLnMock
-        putStrLnMock a = modifyIORef mockConsole (a :)
+    let
+      fooDMock = Dep "fooMock" []
+      fooMock = 33
+    $(assemble $ override "foo" "fooMock" barD) `shouldBe` 34
 
-        -- readMockConsole = readIORef mockConsole >>= fmap reverse
-        readMockConsole = do
-          readIORef mockConsole >>= (reverse .> return)
-          -- readIORef mockConsole $> (fmap reverse)
 
-        -- $(inj)
-        getCurrentTimeMockD = Dep "getCurrentTimeMock" []
-        getCurrentTimeMockT = getCurrentTimeMock
-        getCurrentTimeMock = readIORef cTime
+  specify "type variable support" $ do
 
-        setUpThen cont = do
-          writeIORef mockConsole []
-          writeIORef cTime $ parseTime "2016-01-01 14:00:00"
-          timer <- $(makeTimerD
-              $> override "putStrLn" "putStrLnMock"
-              $> override "getCurrentTime" "getCurrentTimeMock"
-              $> assemble
-            )
-          timer
-          cont timer
+    $(assemble $ idTestD) `shouldBe` 3
 
-      readMockConsole >>= (`shouldBe` [])
+    let
+      idDMock = Dep "idMock" []
+      idMock = (+1)
+    $(assemble $ override "id" "idMock" idTestD) `shouldBe` 4
 
-      setUpThen $ \timer -> do
-        readMockConsole >>= (`shouldBe` ["2016-01-01 14:00:00 UTC"])
 
-      setUpThen $ \timer -> do
+  specify "module support" $ do
+    $(assemble $ testModuleD) `shouldBe` 12
+
+  -- specify "qualified names" $ do
+  --   $(assemble $ Dep "Prelude.id" []) 1 `shouldBe` 1
+  --   $(assemble $ Dep "Prelude.*" []) 2 3 `shouldBe` 6
+
+
+  specify "code that is more real-life" $ do
+
+    let parseTime t =
+          fromJust $ parseTimeM True defaultTimeLocale "%Y-%m-%d %H:%M:%S%Q" t
+    mockConsole <- newIORef []
+    cTime <- newIORef $ parseTime "2016-01-01 14:00:00"
+    let
+      -- $(inj)
+      putStrLnMockD = Dep "putStrLnMock" []
+      putStrLnMockT = putStrLnMock
+      putStrLnMock a = modifyIORef mockConsole (a :)
+
+      -- readMockConsole = readIORef mockConsole >>= fmap reverse
+      readMockConsole = do
+        readIORef mockConsole >>= (reverse .> return)
+        -- readIORef mockConsole $> (fmap reverse)
+
+      -- $(inj)
+      getCurrentTimeMockD = Dep "getCurrentTimeMock" []
+      getCurrentTimeMockT = getCurrentTimeMock
+      getCurrentTimeMock = readIORef cTime
+
+      setUpThen cont = do
+        writeIORef mockConsole []
+        writeIORef cTime $ parseTime "2016-01-01 14:00:00"
+        timer <- $(makeTimerD
+            $> override "putStrLn" "putStrLnMock"
+            $> override "getCurrentTime" "getCurrentTimeMock"
+            $> assemble
+          )
         timer
-        readMockConsole >>= (`shouldBe`
-          ["2016-01-01 14:00:00 UTC", "2016-01-01 14:00:00 UTC, diff: 0s"])
+        cont timer
 
-      setUpThen $ \timer -> do
-        writeIORef cTime $ parseTime "2016-01-01 14:00:01"
-        timer
-        readMockConsole >>= (`shouldBe`
-          [ "2016-01-01 14:00:00 UTC", "2016-01-01 14:00:01 UTC, diff: 1s"])
+    readMockConsole >>= (`shouldBe` [])
 
-      -- [x] TODO figure out a way to branch out just like with Jasmine-Given JS testing framework
-      setUpThen $ \timer -> do
-        writeIORef cTime $ parseTime "2016-01-01 14:00:00.00002"
-        timer
-        readMockConsole >>= (`shouldBe`
-          [ "2016-01-01 14:00:00 UTC", "2016-01-01 14:00:00.00002 UTC, diff: 0.00002s"])
+    setUpThen $ \timer -> do
+      readMockConsole >>= (`shouldBe` ["2016-01-01 14:00:00 UTC"])
+
+    setUpThen $ \timer -> do
+      timer
+      readMockConsole >>= (`shouldBe`
+        ["2016-01-01 14:00:00 UTC", "2016-01-01 14:00:00 UTC, diff: 0s"])
+
+    setUpThen $ \timer -> do
+      writeIORef cTime $ parseTime "2016-01-01 14:00:01"
+      timer
+      readMockConsole >>= (`shouldBe`
+        [ "2016-01-01 14:00:00 UTC", "2016-01-01 14:00:01 UTC, diff: 1s"])
+
+    -- [x] TODO figure out a way to branch out just like with Jasmine-Given JS testing framework
+    setUpThen $ \timer -> do
+      writeIORef cTime $ parseTime "2016-01-01 14:00:00.00002"
+      timer
+      readMockConsole >>= (`shouldBe`
+        [ "2016-01-01 14:00:00 UTC", "2016-01-01 14:00:00.00002 UTC, diff: 0.00002s"])
 
 
 
-    specify "automatic deps declaration" $ do
-      -- deps "makeTimer" $> runQ >>= (pprint  .> (`shouldBe` "Dep \"makeTimer\" [putStrLnD, getCurrentTimeD]"))
-      -- putStrLn $( (fmap show $ location) >>= ( StringL .> LitE .> return)  )
-      -- let asd = fmap (LitE $ StringL) getContentOfNextLine
-      strip $(getContentOfNextLineLit) `shouldBe` "let asd foo = foo + 1"
-      let asd foo = foo + 1
-      strip $(getContentOfNextLineLit) `shouldBe` "let asd foo = foo + 2"
-      let asd foo = foo + 2
-      let
-        a = strip $(getContentOfNextLineLit) `shouldBe` "asd foo = foo + 2"
-        asd foo = foo + 2
-      a
-      -- parseLineToDeps "foo = 1" `shouldBe` Dep "foo" []
+  specify "automatic deps declaration" $ do
+    -- deps "makeTimer" $> runQ >>= (pprint  .> (`shouldBe` "Dep \"makeTimer\" [putStrLnD, getCurrentTimeD]"))
+    -- putStrLn $( (fmap show $ location) >>= ( StringL .> LitE .> return)  )
+    -- let asd = fmap (LitE $ StringL) getContentOfNextLine
+    strip $(getContentOfNextLineLit) `shouldBe` "let asd foo = foo + 1"
+    let asd foo = foo + 1
+    strip $(getContentOfNextLineLit) `shouldBe` "let asd foo = foo + 2"
+    let asd foo = foo + 2
+    let
+      a = strip $(getContentOfNextLineLit) `shouldBe` "asd foo = foo + 2"
+      asd foo = foo + 2
+    a
+    -- parseLineToDeps "foo = 1" `shouldBe` Dep "foo" []
 
-      parseLineToDeps "a = 1" `shouldBe` ("a", "aD", [], [])
-      parseLineToDeps "b = 1" `shouldBe` ("b", "bD", [], [])
-      parseLineToDeps "b a = 1" `shouldBe` ("b", "bD", ["aD"], ["a"])
+    parseLineToDeps "a = 1" `shouldBe` ("a", "aD", [], [])
+    parseLineToDeps "b = 1" `shouldBe` ("b", "bD", [], [])
+    parseLineToDeps "b a = 1" `shouldBe` ("b", "bD", ["aD"], ["a"])
 
-      (injectableI (return "asd = 2") $> runQ $> fmap pprint) >>=
-        (`shouldSatisfy` ("asdD = Dep \"asd\" []" `isPrefixOf`))
-      (injectableI (return "asd a = 2") $> runQ $> fmap pprint) >>=
-        (`shouldSatisfy` ("asdD = Dep \"asd\" [aD]" `isPrefixOf`))
+    (injectableI (return "asd = 2") $> runQ $> fmap pprint) >>=
+      (`shouldSatisfy` ("asdD = Dep \"asd\" []" `isPrefixOf`))
+    (injectableI (return "asd a = 2") $> runQ $> fmap pprint) >>=
+      (`shouldSatisfy` ("asdD = Dep \"asd\" [aD]" `isPrefixOf`))
 
-      (injLeaf "asdasd" $> runQ $> fmap pprint) >>=
-        (`shouldSatisfy` ("asdasdD = Dep \"asdasd\" []" `isPrefixOf`))
+    (injLeaf "asdasd" $> runQ $> fmap pprint) >>=
+      (`shouldSatisfy` ("asdasdD = Dep \"asdasd\" []" `isPrefixOf`))
 
-      return ()
+    return ()
 
   describe "idiomatic module support" $ do
     specify "utils" $ do
