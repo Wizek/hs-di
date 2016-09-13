bad22f620d145eedbc6b2a61a79d51efba8ff34f
Milán Nagy <123.wizek@gmail.com>
2016-09-14 01:12:05 +0200

    +Assert: breakthrough

---
+++ test/Assert.hs
@@ -0,0 +1,19 @@
+{-# OPTIONS_GHC -fno-warn-missing-fields #-}
+
+module Assert where
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote
+import Language.Haskell.Meta
+import ComposeLTR
+import Data.String.Utils
+-- import Language.Haskell.QuasiQuotes
+
+aa :: QuasiQuoter
+aa = QuasiQuoter { quoteExp = f }
+
+f :: String -> Q Exp
+f ss = [| $(n "it") $(return $ LitE $ StringL s) $ $(parseExp s $> either error return) |]
+  where
+  s = strip ss
+n s = return $ VarE $ mkName s
