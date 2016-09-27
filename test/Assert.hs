5b195de8530063b078f1fe784b3b23a49714e2cb
Milán Nagy <123.wizek@gmail.com>
2016-09-27 13:13:56 +0200

    Added poc for specify level code inclusion

--- test/Assert.hs
+++ test/Assert.hs
@@ -1,20 +1,31 @@
 {-# OPTIONS_GHC -fno-warn-missing-fields #-}
 
-module Assert (aa) where
+module Assert (aa, ab) where
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
 import Language.Haskell.Meta
 import ComposeLTR
 import Data.String.Utils
+import Data.Monoid
 -- import Language.Haskell.QuasiQuotes
 
 aa :: QuasiQuoter
 aa = QuasiQuoter { quoteExp = f }
-
-f :: String -> Q Exp
-f ss = [| $(n "specify") $(return $ LitE $ StringL s) $ $(parseExp s $> either error return) |]
   where
-  s = strip ss
+  f :: String -> Q Exp
+  f ss = do
+    [| $(n "specify") $(return $ LitE $ StringL s) $ $(parseExp s $> either error return) |]
+    where
+    s = strip ss
+
+
+ab :: QuasiQuoter
+ab = QuasiQuoter { quoteExp = f }
+  where
+  f :: String -> Q Exp
+  f ss = [| $(parseExp s $> either error return) $> $(n "failDetails") ("\nAssert: " <> $(n "singleLineOrIndent") $(stringE s)) |]
+    where
+    s = strip ss
 
 n s = return $ VarE $ mkName s
